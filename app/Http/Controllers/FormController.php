1c22406b8875e2229c1725726bbd7e7ca9985cbc
root <alvin.faiz@primakom.co.id>
2022-01-04 12:09:47 +0700

    pengurutan form fasilitas

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -222,7 +222,30 @@ class FormController extends Controller
                     ELSE 7
                     END ASC
                     ");
-                }])->where('status', 1)->where('kategori', $dataKategori->kode)->where('form_jenis', env('FORM_FACILITIES'))->orderBy('kategori', 'asc')->orderBy('judul', 'asc')->get();
+                }])->where('status', 1)->where('kategori', $dataKategori->kode)->where('form_jenis', env('FORM_FACILITIES'))->orderBy('kategori', 'asc')->orderByRaw(
+                    "CASE 
+                    WHEN judul like 'PAC 1 T%' THEN 2
+                    WHEN judul like 'PAC 1 H%' THEN 3
+                    WHEN judul like 'PAC 2 T%' THEN 4
+                    WHEN judul like 'PAC 2 H%' THEN 5
+                    WHEN judul like 'PAC 3 T%' THEN 6
+                    WHEN judul like 'PAC 3 H%' THEN 7
+                    WHEN judul like 'PAC 4 T%' THEN 8
+                    WHEN judul like 'PAC 4 H%' THEN 9
+                    WHEN judul like 'PAC 5 T%' THEN 10
+                    WHEN judul like 'PAC 5 H%' THEN 11
+                    WHEN judul like 'UPS 1 V%' THEN 12
+                    WHEN judul like 'UPS 1 A%' THEN 13
+                    WHEN judul like 'UPS 1 L%' THEN 14
+                    WHEN judul like 'UPS 2 V%' THEN 15
+                    WHEN judul like 'UPS 2 A%' THEN 16
+                    WHEN judul like 'UPS 2 L%' THEN 17
+                    WHEN judul like 'UPS APC V%' THEN 18
+                    WHEN judul like 'UPS APC I%' THEN 19
+                    WHEN judul like 'UPS APC R%' THEN 20
+                    ELSE 1
+                    END ASC"
+                )->orderBy('judul', 'asc')->get();
 
                 $form = $form->map(function ($dataForm) {
                     $form = [];
